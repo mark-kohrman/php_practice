78be8d7a742b3092df08527464df53ee81afba26
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-13 13:35:51 -0700

    add while loop

---
+++ whileloops.php
@@ -0,0 +1,65 @@
+<?php
+# 1. Write a while loop to print the numbers 1 through 10.
+$number = 1;
+while ($number <= 10) {
+  // echo $number;
+  // echo "\n";
+  $number += 1;
+}
+# 2. Write a while loop that prints the word "hello" 5 times.
+$word = "hello";
+$i = 0;
+while ($i < 5) {
+  // echo $word;
+  // echo "\n";
+  $i += 1;
+}
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+// while (true) {
+//   // $word = readline("Please enter word or stop when your done: ");
+//   if ($word == "stop") {
+//     break;
+//   }
+// }
+
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+// $number = 0;
+// while ($number <= 100) {
+//   echo $number;
+//   echo "\n";
+//   $number += 5;
+// }
+# 5. Write a while loop that prints the number 9000 ten times.
+// $i = 0;
+// while ($i < 10) {
+//   echo 9000;
+//   echo "\n";
+//   $i += 1;
+// }
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+// $number = 50;
+// while ($number <= 70) {
+//   echo $number;
+//   echo "\n";
+//   $number += 1;
+// }
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+while (true) {
+  $word = readline("Please enter a word: ");
+  if (strlen($word) > 5) {
+    break;
+  }
+}
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
