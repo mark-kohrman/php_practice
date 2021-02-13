35a08ec147313e5e3d1fd0ed0106699203b4bec9
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-13 12:39:31 -0700

    add conditional practice

---
+++ conditionals.php
@@ -0,0 +1,51 @@
+<?php
+
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+// $x = 1;
+// if ($x == 10) {
+//   echo 0;
+// } else {
+//   echo 1;
+// }
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+// $y = 10;
+// $x = 4;
+// if ($x < 10 && $y < 10) {
+//   echo 1;
+// } else {
+//   echo 0;
+// }
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+// $var = 99;
+// if ($var < 0) {
+//   echo 1776;
+// } else {
+//   echo 1979;
+// }
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+$number = 41;
+if ($number > 80) {
+  echo 5;
+} elseif ($number > 60) {
+  echo 4;
+} elseif ($number > 40) {
+  echo 3;
+} elseif ($number > 20) {
+  echo 2;
+} else {
+  echo 1;
+}
