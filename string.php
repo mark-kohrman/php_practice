bfaeb9fbd0370f40c011ab76a454cc610fe5fa96
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-13 13:05:30 -0700

    adding string interpolation and concatenation

---
+++ string.php
@@ -0,0 +1,43 @@
+<?php
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+// $first_name = "Bob";
+// $last_name = "Jones";
+// echo $first_name . " " . $last_name;
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+// $first_name = "Bob";
+// $last_name = "Jones";
+// echo "{$first_name} {$last_name}";
+
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+// $title = "Sapiens";
+// $author = "Yuval Noah Harari";
+// echo "The author of " . $title . " is " . $author . ".";
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+// $password = readline("Enter a password: ");
+// if ($password == "Joshua") {
+//   echo "Shall we play a game?";
+// } else {
+//   echo "Acces denied";
+// }
+
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+$city_one = "Chicago";
+$city_two = "Melbourne";
+$city_three = "Wuhan";
+
+echo "Hello, my favorite city is {$city_one}.  I've been to {$city_two} and it was fun.  I also like {$city_three} but I got injured";
