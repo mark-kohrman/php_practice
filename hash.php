ba682fa64633e2d5fbe8146b5ab39800fe41ab08
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-13 15:41:00 -0700

    add hash practice

---
+++ hash.php
@@ -0,0 +1,36 @@
+<?php
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+// $person = array("firstName" => "Jeff", "lastName" => "Wilson", "email" => "jeff@aol.com");
+// echo $person["firstName"] . "\n";
+// echo $person["lastName"] . "\n";
+// echo $person["email"];
+
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+// $people = array(["firstName" => "Jeff", "lastName" => "Wilson"], ["firstName" => "Joe", "lastName" => "Lee"], ["firstName" => "Ana", "lastName" => "Axe"]);
+// print_r($people[0]);
+
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+$people = array(["title" => "Jeff", "author" => "Lee Hanna"], ["title" => "Sapiens", "author" => "Harari"], ["title" => "War and Peace", "author" => "Tolstoy"]);
+print_r($people[2]["author"]);
+
+# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+$words = ["key" => "unlock with it", "coffee" => "wake up with it"];
+$words["lime"] = "put beer in it";
+print_r($words);
+
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/d257aec07882d78009bd796ed53f81bb
