7682694d124f1617652a19150b05b985e3036851
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-13 13:18:14 -0700

    add methods practice

---
+++ methods.php
@@ -0,0 +1,32 @@
+<?php
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+// $word = readline("Please enter a word: ");
+// echo strtoupper($word);
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+// $number = readline("Please enter a number: ");
+// if ($number > 100) {
+//   echo "That's a big number";
+// }
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+// $number_one = readline("Please enter first number: ");
+// $number_two = readline("Please enter second number: ");
+// echo $number_one + $number_two;
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+// $word = readline("Please enter a word: ");
+// echo strrev($word);
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
