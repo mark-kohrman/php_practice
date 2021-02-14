455e65b91002e142b91dec7490ce3e086369d73b
Mark-Kohrman <mark.kohrman@gmail.com>
2021-02-14 12:10:53 -0700

    add conditional array practice

---
+++ if_arrays.php
@@ -0,0 +1,70 @@
+<?php
+
+#  1. Start with an array of numbers and create a new array with only the numbers less than 20.
+#     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+// $numbers = [2, 32, 80, 18, 12, 3];
+// $less_than = array();
+
+// for ($i = 0; $i <= count($numbers); $i++) {
+//   if ($numbers[$i] < 20) {
+//     array_push($less_than, $numbers[$i]);
+//   }
+// }
+// print_r($less_than);
+// echo count($less_than);
+
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+$words = ["winner", "winner", "chicken", "dinner"];
+$w_words = [];
+$i = 0;
+
+while ($i < count($words)) {
+  if ($words[$i][0] == "w") {
+    array_push($w_words, $words[$i]);
+  }
+  $i++;
+}
+// print_r($w_words);
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+// $items = [["name" => "chair", "price" => 100], ["name" => "pencil", "price" => 1], ["name" => "book", "price" => 4]];
+
+// $i = 0;
+// $expensive = [];
+
+// while ($i <  count($items)) {
+//   $item = $items[$i];
+//   $item_price = $items[$i]["price"];
+//   if ($item_price > 5) {
+//     array_push($expensive, $item);
+//   }
+//   $i++;
+// }
+// print_r($expensive);
+#  4. Start with an array of numbers and create a new array with only the even numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+#  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+#     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+#  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+#     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+# 10. Start with an array of numbers and create a new array with only the odd numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+
+
+# SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
+# SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/a702894841c7018ed8c127b647ae21f8
+# SOLUTIONS (using .select shortcut): https://gist.github.com/peterxjang/b8c8fb8b77b2cae7bb9cc62a3a434761
